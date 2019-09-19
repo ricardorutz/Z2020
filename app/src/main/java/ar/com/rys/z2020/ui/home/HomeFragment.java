6afd34745751e0709cdbe1425d21229f15983aca
rysio <ricardo.rutz@gmail.com>
2019-09-19 16:01:38 -0300

    cambie el home - test

--- app/src/main/java/ar/com/rys/z2020/ui/home/HomeFragment.java
+++ app/src/main/java/ar/com/rys/z2020/ui/home/HomeFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,17 +14,26 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+
+import java.util.ArrayList;
+
+
 import ar.com.rys.z2020.R;
+
+//http://www.technotalkative.com/android-bullets-in-listview/
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    ArrayList<String> listCountry;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        /*
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
@@ -30,6 +41,41 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+
+         */
+
+
+
+        prepareList();
+        ListView listView = root.findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<String>(
+                root.getContext(), R.layout.list_item, listCountry));
+
         return root;
+    }
+
+    public void prepareList()
+    {
+        listCountry = new ArrayList<String>();
+        listCountry.add("India");
+        listCountry.add("Brazil");
+        listCountry.add("Canada");
+        listCountry.add("China");
+        listCountry.add("France");
+        listCountry.add("Germany");
+        listCountry.add("Iran");
+        listCountry.add("Italy");
+        listCountry.add("Japan");
+        listCountry.add("Korea");
+        listCountry.add("Mexico");
+        listCountry.add("Netherlands");
+        listCountry.add("Portugal");
+        listCountry.add("Russia");
+        listCountry.add("Saudi Arabia");
+        listCountry.add("Spain");
+        listCountry.add("Turkey");
+        listCountry.add("United Kingdom");
+        listCountry.add("United States");
     }
 }
