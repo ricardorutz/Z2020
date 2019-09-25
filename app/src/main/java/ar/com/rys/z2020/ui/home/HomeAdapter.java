4e9c85568de8c4e2b5fea412362a04b641d48dda
rysio <ricardo.rutz@gmail.com>
2019-09-25 16:10:32 -0300

    background transparente

--- app/src/main/java/ar/com/rys/z2020/ui/home/HomeAdapter.java
+++ app/src/main/java/ar/com/rys/z2020/ui/home/HomeAdapter.java
@@ -1,7 +1,6 @@
 package ar.com.rys.z2020.ui.home;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -38,7 +37,6 @@ public class HomeAdapter extends ArrayAdapter<String> {
             iconResourcesIds.add(R.drawable.ic_cloud_sun_rain_solid);
             iconResourcesIds.add(R.drawable.ic_temperature_high_solid);
             iconResourcesIds.add(R.drawable.ic_temperature_low_solid);
-
         }
 
         return iconResourcesIds;
@@ -53,14 +51,7 @@ public class HomeAdapter extends ArrayAdapter<String> {
 
         TextView textViewMessage = rowView.findViewById(R.id.itemText);
 
-        Log.d("Posicion","I am here" + position);
-
-        //textViewMessage.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_calendar_alt_solid, 0, 0, 0);
         textViewMessage.setCompoundDrawablesWithIntrinsicBounds(iconResourcesIds.get(position), 0, 0, 0);
-
-
-        String formattedText = "This <i>is</i> a <b>test</b> of <a href='http://foo.com'>html</a>";
-        //textViewMessage.setText(Html.fromHtml(formattedText));
         textViewMessage.setText(listOfTexts.get(position));
 
         return rowView;
