6276e8d155b0dc42958b602f8a577e9860f9382d
rysio <ricardo.rutz@gmail.com>
2019-10-01 18:04:46 -0300

    haciedo el album de recorridos

--- app/src/main/java/ar/com/rys/z2020/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/ar/com/rys/z2020/ui/slideshow/SlideshowFragment.java
@@ -1,11 +1,16 @@
 package ar.com.rys.z2020.ui.slideshow;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,104 +18,111 @@ import org.osmdroid.views.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.yanzhenjie.album.Action;
+import com.yanzhenjie.album.Album;
+import com.yanzhenjie.album.AlbumFile;
+import com.yanzhenjie.album.impl.OnItemClickListener;
+import com.yanzhenjie.album.widget.divider.Api21ItemDivider;
+import com.yanzhenjie.album.widget.divider.Divider;
 
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapController;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import ar.com.rys.z2020.R;
 
-public class SlideshowFragment extends Fragment implements OnMapReadyCallback {
+public class SlideshowFragment extends Fragment {
 
-    private MapView myOpenMapView;
-    private MapController myMapController;
-    private GoogleMap mMap;
 
-/*
-    private SlideshowViewModel slideshowViewModel;
+
+    private SlideshowAdapter mAdapter;
+    private ArrayList<AlbumFile> mAlbumFiles = new ArrayList<>();
+
+
+    public  static String[] imagesString = {
+            String.valueOf(R.drawable.trasa_bsas),
+            String.valueOf(R.drawable.trasa_sma),
+            String.valueOf(R.drawable.trasa_wedro),
+
+    };
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(SlideshowViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+
+        final View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+        super.onCreate(savedInstanceState);
+
+        ArrayList<String> imageList = new ArrayList<>();
+        Collections.addAll(imageList, imagesString);
+
+        imageList.forEach(p -> {
+
+            AlbumFile al = new AlbumFile();
+            al.setMediaType(AlbumFile.TYPE_IMAGE);
+            al.setPath(p);
+            mAlbumFiles.add(al);
+
         });
+
+
+        final Context context = this.getContext();
+
+        RecyclerView recyclerView = root.findViewById(R.id.recycler_view);
+
+        //recyclerView.getChildItemId(root.findViewById(R.id.textView3));
+
+        recyclerView.setLayoutManager(new GridLayoutManager(context, 1));
+        Divider divider = new Api21ItemDivider(Color.TRANSPARENT, 10, 10);
+        recyclerView.addItemDecoration(divider);
+
+        mAdapter = new SlideshowAdapter(context, new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                previewImage(position);
+            }
+        }, mAlbumFiles);
+        recyclerView.setAdapter(mAdapter);
+
+
+
+
+
+
+
         return root;
     }
 
- */
 
-    public SlideshowFragment() {
-        // Required empty public constructor
+    private void previewImage(int position) {
+        if (mAlbumFiles == null || mAlbumFiles.size() == 0) {
+            //Toast.makeText(this, R.string.no_selected, Toast.LENGTH_LONG).show();
+        } else {
+            Album.galleryAlbum(this)
+                    .checkable(false)
+                    .checkedList(mAlbumFiles)
+                    .currentPosition(position)
+                    /*
+                    .widget(
+                            Widget.newDarkBuilder(this)
+                                    .title(mToolbar.getTitle().toString())
+                                    .build()
+                    )
+                    */
+                    .onResult(new Action<ArrayList<AlbumFile>>() {
+                        @Override
+                        public void onAction(@NonNull ArrayList<AlbumFile> result) {
+                            mAlbumFiles = result;
+                            mAdapter.notifyDataSetChanged(mAlbumFiles);
+                            // mTvMessage.setVisibility(result.size() > 0 ? View.VISIBLE : View.GONE);
+                        }
+                    })
+                    .start();
+        }
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_slideshow, container, false);
-/*
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
-
-        mapFragment.getMapAsync(this);
-
-
- */
-        GeoPoint madrid = new GeoPoint(40.416775, -3.70379);
-
-        myOpenMapView = rootView.findViewById(R.id.openmapview);
-        myOpenMapView.setBuiltInZoomControls(true);
-        myMapController = (MapController) myOpenMapView.getController();
-        myMapController.setCenter(madrid);
-        myMapController.setZoom(6);
-
-        myOpenMapView.setMultiTouchControls(true);
-
-
-        return rootView;
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        googleMap.clear(); //clear old markers
-
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-
-
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-
-
-
 }
