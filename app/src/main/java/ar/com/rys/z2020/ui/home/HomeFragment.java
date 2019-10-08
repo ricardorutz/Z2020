aa3651cecc11710eeea338a040d891bf2f865ba4
ricardo.rutz <ricardo.rutz@sbi-technology.com>
2019-10-08 18:07:34 -0300

    countdown de dias.

--- app/src/main/java/ar/com/rys/z2020/ui/home/HomeFragment.java
+++ app/src/main/java/ar/com/rys/z2020/ui/home/HomeFragment.java
@@ -1,20 +1,32 @@
 package ar.com.rys.z2020.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 
+import com.asp.fliptimerviewlibrary.CountDownClock;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
+import ar.com.rys.z2020.MainActivity;
 import ar.com.rys.z2020.R;
+import ar.com.rys.z2020.ui.home.HomeAdapter;
+import cn.iwgang.countdownview.CountdownView;
+import ticker.views.com.ticker.widgets.circular.timer.callbacks.CircularViewCallback;
+import ticker.views.com.ticker.widgets.circular.timer.view.CircularView;
 
 //http://www.technotalkative.com/android-bullets-in-listview/
 
@@ -32,7 +44,20 @@ public class HomeFragment extends Fragment {
         ArrayAdapter arrayAdapter = new ArrayAdapter<Integer>(
                 root.getContext(), R.layout.fragment_home_list_item, listOfTexts);
 
+        Calendar today = Calendar.getInstance();
 
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, 2020);
+        cal.set(Calendar.MONTH, Calendar.JANUARY);
+        cal.set(Calendar.DAY_OF_MONTH, 30);
+
+
+
+        long timeTo = cal.getTimeInMillis()-today.getTimeInMillis();
+
+
+        CountdownView mCvCountdownView = root.findViewById(R.id.cv_countdownViewTest1);
+        mCvCountdownView.start(995550000); // Millisecond
 
         HomeAdapter nAdapter = new HomeAdapter(root.getContext(), R.layout.fragment_home_list_item, listOfTexts);
 
