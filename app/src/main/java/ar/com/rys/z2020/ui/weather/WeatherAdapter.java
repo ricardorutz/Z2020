02ff035d6f1556a52167cdf946b38e801fcedcd8
profe <profesorlider.sbi@mail.com>
2019-10-22 17:04:05 -0300

    clima

---
+++ app/src/main/java/ar/com/rys/z2020/ui/weather/WeatherAdapter.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2016 Yan Zhenjie.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package ar.com.rys.z2020.ui.weather;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.yanzhenjie.album.AlbumFile;
+import com.yanzhenjie.album.impl.OnItemClickListener;
+
+import java.util.List;
+
+import ar.com.rys.z2020.R;
+
+/**
+ * <p>Image adapter.</p>
+ * Created by Yan Zhenjie on 2016/10/30.
+ */
+public class WeatherAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private LayoutInflater mInflater;
+    private OnItemClickListener mItemClickListener;
+
+    private List<WeatherResourceData> trasyResourceDataList;
+
+    public WeatherAdapter(Context context, OnItemClickListener onItemClickListener, List<WeatherResourceData> mAlbumFiles) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mItemClickListener = onItemClickListener;
+        this.trasyResourceDataList = mAlbumFiles;
+    }
+
+    public void notifyDataSetChanged(List<WeatherResourceData> imagePathList) {
+        this.trasyResourceDataList = imagePathList;
+        super.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return AlbumFile.TYPE_IMAGE;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        switch (viewType) {
+            case AlbumFile.TYPE_IMAGE: {
+                return new WeatherViewHolder(mInflater.inflate(R.layout.fragment_weather_item_content_image, parent, false), mItemClickListener);
+            }
+            default: {
+                throw new AssertionError("This should not be the case.");
+            }
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        ((WeatherViewHolder) holder).setData(trasyResourceDataList.get(position).getAlbumFile(), trasyResourceDataList.get(position).getImageTitle());
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return trasyResourceDataList == null ? 0 : trasyResourceDataList.size();
+
+    }
+
+
+}
---
+++ app/src/main/java/ar/com/rys/z2020/ui/weather/WeatherResourceData.java
@@ -0,0 +1,37 @@
+package ar.com.rys.z2020.ui.weather;
+
+import com.yanzhenjie.album.AlbumFile;
+
+public class WeatherResourceData {
+
+    private int imagesResource;
+    private String imageTitle;
+    private AlbumFile albumFile;
+
+    public String getImagesResource() {
+        return String.valueOf(imagesResource);
+    }
+
+    public void setImagesResource(int imagesResource) {
+        this.imagesResource = imagesResource;
+    }
+
+    public String getImageTitle() {
+        return String.valueOf(imageTitle);
+    }
+
+    public void setImageTitle(String imageTitle) {
+        this.imageTitle = imageTitle;
+    }
+
+    public AlbumFile getAlbumFile(){
+
+        if(albumFile==null){
+            albumFile = new AlbumFile();
+            albumFile.setMediaType(AlbumFile.TYPE_IMAGE);
+            albumFile.setPath(getImagesResource());
+        }
+
+        return albumFile;
+    }
+}
