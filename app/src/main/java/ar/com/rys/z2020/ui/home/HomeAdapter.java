e68ccfbecf3f8f24fb23acff073217ee07121025
rysio <ricardo.rutz@gmail.com>
2019-10-23 21:59:17 -0300

    cambie el home

--- app/src/main/java/ar/com/rys/z2020/ui/home/HomeAdapter.java
+++ app/src/main/java/ar/com/rys/z2020/ui/home/HomeAdapter.java
@@ -16,43 +16,25 @@ import ar.com.rys.z2020.R;
 
 public class HomeAdapter extends ArrayAdapter<String> {
     private Context context;
-    private List<Integer> listOfTexts;
-    private List<Integer> iconResourcesIds;
 
 
-
-    public HomeAdapter(Context context, int layautResource, List<Integer> listOfTexts) {
-        super(context, layautResource, listOfTexts.stream().map(s -> String.valueOf(s)).collect(Collectors.toList()));
+    public HomeAdapter(Context context, int layautResource) {
+        super(context, layautResource);
         this.context = context;
-        this.listOfTexts = listOfTexts;
-        this.iconResourcesIds= initResources();
     }
 
-    private List<Integer> initResources() {
-
-        if(iconResourcesIds == null){
-            iconResourcesIds = new ArrayList();
-            iconResourcesIds.add(R.drawable.ic_age);
-            iconResourcesIds.add(R.drawable.ic_cloud_sun_rain_solid);
-            iconResourcesIds.add(R.drawable.ic_temperature_high_solid);
-            iconResourcesIds.add(R.drawable.ic_temperature_low_solid);
-        }
-
-        return iconResourcesIds;
-
-    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
         View rowView = inflater.inflate(R.layout.fragment_home_list_item, parent, false);
 
-        TextView textViewMessage = rowView.findViewById(R.id.itemText);
-
-        textViewMessage.setCompoundDrawablesWithIntrinsicBounds(iconResourcesIds.get(position), 0, 0, 0);
-        textViewMessage.setText(listOfTexts.get(position));
 
         return rowView;
     }
+
+
 }
--- app/src/main/java/ar/com/rys/z2020/ui/home/HomeFragment.java
+++ app/src/main/java/ar/com/rys/z2020/ui/home/HomeFragment.java
@@ -1,23 +1,13 @@
 package ar.com.rys.z2020.ui.home;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
-
-
-
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 
 
 
@@ -30,17 +20,11 @@ import cn.iwgang.countdownview.CountdownView;
 
 public class HomeFragment extends Fragment {
 
-    List<Integer> listOfTexts;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        listOfTexts = prepareList();
-
-        ArrayAdapter arrayAdapter = new ArrayAdapter<Integer>(
-                root.getContext(), R.layout.fragment_home_list_item, listOfTexts);
 
         Calendar today = Calendar.getInstance();
 
@@ -50,14 +34,13 @@ public class HomeFragment extends Fragment {
         cal.set(Calendar.DAY_OF_MONTH, 30);
 
 
-
         long timeTo = cal.getTimeInMillis()-today.getTimeInMillis();
 
 
         CountdownView mCvCountdownView = root.findViewById(R.id.cv_countdownView);
         mCvCountdownView.start(timeTo); // Millisecond
 
-        HomeAdapter nAdapter = new HomeAdapter(root.getContext(), R.layout.fragment_home_list_item, listOfTexts);
+        HomeAdapter nAdapter = new HomeAdapter(root.getContext(), R.layout.fragment_home_list_item);
 
 
         ListView listView = root.findViewById(R.id.listView);
@@ -67,19 +50,5 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    public List<Integer> prepareList()
-    {
 
-        if(listOfTexts==null){
-            listOfTexts = new ArrayList<Integer>();
-            listOfTexts.add(R.string.home_item_mayor);
-            listOfTexts.add(R.string.home_item_temperature);
-            listOfTexts.add(R.string.home_item_temperature_bsas);
-            listOfTexts.add(R.string.home_item_temperature_sma);
-
-        }
-
-        return  listOfTexts;
-
-    }
 }
